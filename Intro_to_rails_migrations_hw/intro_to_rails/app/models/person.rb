868dc79566120fb850220f121f5df45531a9b73c
miker704 <miker704@gmail.com>
2022-05-03 03:11:06 -0400

    finished intro to rails

---
+++ Intro_to_rails_migrations_hw/intro_to_rails/app/models/person.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  house_id   :integer          not null
+#
+class Person < ApplicationRecord
+    validates :name, presence: true
+    validates :house_id, presence: true
+
+    belongs_to(
+        :house,
+        primary_key: :id,
+        foreign_key: :house_id,
+        class_name: :House
+    )
+
+
+end
