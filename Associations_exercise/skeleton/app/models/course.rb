5aa278e85c05cd69c74d6ac8e50bf3cbd5fcc47f
miker704 <miker704@gmail.com>
2022-05-03 03:55:52 -0400

    fininshed associations exerecise

--- Associations_exercise/skeleton/app/models/course.rb
+++ Associations_exercise/skeleton/app/models/course.rb
@@ -1,2 +1,38 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  instructor_id :integer
+#  prereq_id     :integer
+#
 class Course < ApplicationRecord
+
+        # belongs_to :users,
+        # class_name: :User,
+        # foreign_key: :
+
+        has_many :enrollments,
+        foreign_key: :course_id,
+        class_name: :Enrollment,
+        primary_key: :id
+
+        has_many :enrolled_students,
+        through: :enrollments,
+        source: :users
+        
+        belongs_to :prerequisite,
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        primary_key: :id,
+        optional: true
+
+        belongs_to :instructor,
+        foreign_key: :instructor_id,
+        primary_key: :id,
+        class_name: :User
+
 end
--- Associations_exercise/skeleton/app/models/user.rb
+++ Associations_exercise/skeleton/app/models/user.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+        has_many :enrollments,
+        foreign_key: :student_id,
+        class_name: :Enrollment,
+        primary_key: :id
+
+
+        has_many :enrolled_courses,
+        through: :enrollments,
+        source: :courses
+
+
 end
