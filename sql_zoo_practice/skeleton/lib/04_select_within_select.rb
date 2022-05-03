c32d38fdfb4cb9c19498f3880ab85c436d3da8db
miker704 <miker704@gmail.com>
2022-05-02 20:48:05 -0400

    completed 04_select_within_select.rb

--- sql_zoo_practice/skeleton/lib/04_select_within_select.rb
+++ sql_zoo_practice/skeleton/lib/04_select_within_select.rb
@@ -35,6 +35,14 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  select name
+  from countries
+  where population > (
+    select population
+    from countries
+    where name = 'Russia'
+  );
+
   SQL
 end
 
@@ -42,6 +50,13 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  select name
+  from countries
+  where continent= 'Europe' AND (gdp/population) > (
+    select (gdp/population)
+    from countries
+    where name = 'United Kingdom'
+  ) ;
   SQL
 end
 
@@ -49,6 +64,18 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  select name , continent
+  from countries
+  where continent = (
+    select continent
+    from countries
+    where name = 'Belgium'
+  )
+  OR continent =(
+    select continent
+    from countries
+    where name = 'Belize'
+  );
   SQL
 end
 
@@ -56,6 +83,19 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  select name, population
+  from countries
+  where population > (
+    select population 
+    from countries
+    where name = 'Canada'
+  )
+  and population < (
+    select population 
+    from countries
+    where name = 'Poland'
+  );
+
   SQL
 end
 
@@ -65,5 +105,15 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  
+    select name, continent, population
+    from countries
+    where continent not in (
+      select continent
+      from countries
+      where population > 25000000
+    );
+  
+  
   SQL
 end
